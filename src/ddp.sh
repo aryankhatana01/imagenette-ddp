a787a1119e9fc58cac115498b0286c5f3bd19b79
Aryan Khatana <51334629+aryankhatana01@users.noreply.github.com>
2021-10-15 11:47:40 +0530

    Update ddp.sh

--- src/ddp.sh
+++ src/ddp.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-NUM_PROC=$1
+echo "Number of GPUs: "
+read n_gpu
+NUM_PROC=$n_gpu
 shift
 launcher=$(python -c "from os import path; import torch; print(path.join(path.dirname(torch.__file__), 'distributed', 'launch.py'))")
 python $launcher --nnode=1 --node_rank=0 --nproc_per_node=$NUM_PROC train_ddp.py --local_world_size=$NUM_PROC
